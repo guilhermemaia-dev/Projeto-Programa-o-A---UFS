424c52c6b185e484560de3369709e8f16337a83a
Cauã <cauasantana54@gmail.com>
2026-07-02 09:40:49 -0300

    Criação Inicial da classe Figuras

---
+++ trabalho/Etapa2/figuras.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from abc import ABC, abstractmethod
+#Criação da classe pai (molde) denominada Figuras 
+class Figuras(ABC):
+    def __init__(self, ini_x, ini_y, posx, posy):
+        self.ini_x = ini_x
+        self.ini_y = ini_y
+        self.posx = posx
+        self.posy = posy
+
+#Cria o método abstrato desenhar, que as classes filhas terão que completar
+    @abstractmethod
+    def desenhar(self, canvas):
+        pass
+
