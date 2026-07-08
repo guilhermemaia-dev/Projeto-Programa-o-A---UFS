e6cea1974c054453571e54c06ef69ebdd855edb0
Cauã <cauasantana54@gmail.com>
2026-07-08 07:31:40 -0300

    Adição das classes do model mão-livre e reta

---
+++ model/reta.py
@@ -0,0 +1,7 @@
+class Reta(Figuras):
+    def __init__(self, ini_x, ini_y, posx, posy, cor):
+        super().__init__(ini_x, ini_y, posx, posy)
+        self.cor = cor if cor else "black"
+        
+    def validar(self):
+        return (self.posx - self.ini_x)**2 > 0 or (self.posy - self.ini_y)**2 > 0
