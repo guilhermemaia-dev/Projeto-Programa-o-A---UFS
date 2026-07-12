797097d6200141323e49b4c20405a6d5699be68b
viniciusprudente <viniciusprudentedev@gmail.com>
2026-07-12 03:31:12 -0300

    adição do menu para abrir e salvar arquivos, reorganização dos botões e interface, correção de bugs, adição do botão quadrado

--- view/janela_Paint.py
+++ view/janela_Paint.py
@@ -27,11 +27,19 @@ class JanelaPaint:
         self.canvas = Canvas(self.janela, width=1024, height=576, bg="white")
         self.canvas.pack(pady=20)
 
+        #cria o frame para armazenar melhor os botoes e labels
+        frame_linha1 = Frame(self.janela)
+        frame_linha1.pack()
+        frame_linha2 = Frame(self.janela)
+        frame_linha2.pack()
+        frame_linha3 = Frame(self.janela)
+        frame_linha3.pack()
+
         self.estado_marcador = IntVar(value=0)
-        self.marcador = Checkbutton(self.janela, text="Escolher preenchimento", variable=self.estado_marcador)
+        self.marcador = Checkbutton(frame_linha1, text="Escolher preenchimento", variable=self.estado_marcador)
         self.marcador.pack(side=LEFT, padx=10)
 
-        label_seletor_cor = Label(self.janela, text="SELETOR DE CORES")
+        label_seletor_cor = Label(frame_linha1, text="SELETOR DE CORES")
         label_seletor_cor.pack(side=LEFT, padx=10)
 
         self.canvas.bind("<Button-1>", self.controller.mouse_ini)    
@@ -44,9 +52,9 @@ class JanelaPaint:
         #cria os botões do seletor de cores e quando clica, manda uma mensagem para o controller para informar a cor clicada e o estado do marcador
         for cor in cores:
             if cor == "#E7E7E7":
-                bot_cor = Button(self.janela, width=2, height=1, highlightbackground="black", command=lambda COR='': self.controller.receberAcor(COR, self.estado_marcador.get()))
+                bot_cor = Button(frame_linha1, width=2, height=1, highlightbackground="black", command=lambda COR='': self.controller.receberAcor(COR, self.estado_marcador.get()))
             else:
-                bot_cor = Button(self.janela, width=2, height=1,bg=cor, command=lambda COR=cor: self.controller.receberAcor(COR, self.estado_marcador.get()))
+                bot_cor = Button(frame_linha1, width=2, height=1,bg=cor, command=lambda COR=cor: self.controller.receberAcor(COR, self.estado_marcador.get()))
 
             bot_cor.pack(side=LEFT, pady=1)
 
@@ -54,16 +62,18 @@ class JanelaPaint:
         caminho = os.path.join(os.path.dirname(__file__), "maiscores.png")
         self.imagem = PhotoImage(file=caminho)
         self.imagem = self.imagem.subsample(10, 11)
-        bot_mais_cores = Button(self.janela, image=self.imagem,command=self.abrir_seletor_cor)
+        bot_mais_cores = Button(frame_linha1, image=self.imagem,command=self.abrir_seletor_cor)
         bot_mais_cores.pack(side=LEFT)
 
         #criar os botões do seletor de figuras e colocar na janela logo
-        bot_livre = Button(self.janela, text="MÃO LIVRE", command=lambda: self.controller.selecionar_ferramenta("Mao_Livre"))
-        bot_reta = Button(self.janela, text="RETA", command=lambda: self.controller.selecionar_ferramenta("Reta"))
-        bot_retangulo = Button(self.janela, text="RETANGULAR", command=lambda: self.controller.selecionar_ferramenta("Retangulo"))
-        bot_oval = Button(self.janela, text="OVAL", command=lambda: self.controller.selecionar_ferramenta("Oval"))
-        bot_circulo = Button(self.janela, text="CIRCULAR", command=lambda: self.controller.selecionar_ferramenta("Circulo"))
-        bot_borracha = Button(self.janela, text="BORRACHA", command=lambda: self.controller.selecionar_ferramenta("Borracha"))
+        bot_livre = Button(frame_linha2, text="MÃO LIVRE", command=lambda: self.controller.selecionar_ferramenta("Mao_Livre"))
+        bot_reta = Button(frame_linha2, text="RETA", command=lambda: self.controller.selecionar_ferramenta("Reta"))
+        bot_retangulo = Button(frame_linha2, text="RETANGULAR", command=lambda: self.controller.selecionar_ferramenta("Retangulo"))
+        bot_oval = Button(frame_linha2, text="OVAL", command=lambda: self.controller.selecionar_ferramenta("Oval"))
+        bot_circulo = Button(frame_linha2, text="CIRCULAR", command=lambda: self.controller.selecionar_ferramenta("Circulo"))
+        bot_quadrado = Button(frame_linha2,text="QUADRADO",command=lambda:self.controller.selecionar_ferramenta("Quadrado"))
+        bot_borracha = Button(frame_linha2, text="BORRACHA", command=lambda: self.controller.selecionar_ferramenta("Borracha"))
+        
 
         #Coloca os botões na janela
         bot_livre.pack(side=LEFT)
@@ -71,38 +81,44 @@ class JanelaPaint:
         bot_retangulo.pack(side=LEFT)
         bot_oval.pack(side=LEFT)
         bot_circulo.pack(side=LEFT)
+        bot_quadrado.pack(side=LEFT)
         bot_borracha.pack(side=LEFT)
+        
 
         #cria o botão para apagar tudo
-        bot_limpar = Button(self.janela, text="LIMPAR",command=self.controller.limpar_tela)
+        bot_limpar = Button(frame_linha2, text="LIMPAR",command=self.controller.limpar_tela)
         bot_limpar.pack(side=LEFT)
 
         # mostra a cor da borda atual na interface
-        label_indicar_cor_borda = Label(self.janela, text="BORDA:")
+        label_indicar_cor_borda = Label(frame_linha1, text="BORDA:")
         label_indicar_cor_borda.pack(side=LEFT, padx=10)
-        self.label_cor_selecionadaBorda = Label(self.janela, bg=self.controller.model.cor_selecionada_borda, width=2, height=1)
+        self.label_cor_selecionadaBorda = Label(frame_linha1, bg=self.controller.model.cor_selecionada_borda, width=2, height=1)
         self.label_cor_selecionadaBorda.pack(side=LEFT, padx=10)
 
         # mostra a cor do preenchimento atual
         cor_preenc_default = self.controller.model.cor_selecionada_preenchimento
         if cor_preenc_default == "":
             cor_preenc_default = "#E7E7E7"
-        label_indicar_cor_preenchimento = Label(self.janela, text="PREENCHIMENTO:")
+        label_indicar_cor_preenchimento = Label(frame_linha1, text="PREENCHIMENTO:")
         label_indicar_cor_preenchimento.pack(side=LEFT, padx=10)
-        self.label_corPreench_selecionada = Label(self.janela, bg=cor_preenc_default, width=2, height=1)
+        self.label_corPreench_selecionada = Label(frame_linha1, bg=cor_preenc_default, width=2, height=1)
         self.label_corPreench_selecionada.pack(side=LEFT, padx=10)
 
         # mostrar a ferramenta selecionada atualmente
-        label_indicar_ferramenta_atual = Label(self.janela, text="Ferramenta selecionada:")
+        label_indicar_ferramenta_atual = Label(frame_linha2, text="Ferramenta selecionada:")
         label_indicar_ferramenta_atual.pack(side=LEFT, padx=10)
-        self.mostrar_ferramenta_atual = Label(self.janela, text=self.controller.model.ferramenta_atual)
+        self.mostrar_ferramenta_atual = Label(frame_linha2, text=self.controller.model.ferramenta_atual)
         self.mostrar_ferramenta_atual.pack(side=LEFT,padx=10)
 
-        # botoes de salvar e abrir arquivos
-        bot_salvar = Button(self.janela, text="SALVAR", command=self.controller.salvar_desenho)
-        bot_abrir = Button(self.janela, text="ABRIR", command=self.controller.abrir_desenho)
-        bot_salvar.pack(side=TOP)
-        bot_abrir.pack(side=TOP)
+        #criar um menu para conseguir implementar os botões de salvar e abrir arquivo
+        menu = Menu(self.janela)
+        self.janela.config(menu=menu)
+        menu_arquivo = Menu(menu, tearoff= 0)
+        menu.add_cascade(label="Arquivo",menu=menu_arquivo)
+        menu_arquivo.add_command(label="Salvar",command=self.controller.salvar_desenho)
+        menu_arquivo.add_command(label="Abrir",command=self.controller.abrir_desenho)
+
+       
         
 
 
