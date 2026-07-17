ede5ee00707d752a7b4a632a48634d8b5f632dd3
viniciusprudente <viniciusprudentedev@gmail.com>
2026-07-17 15:44:22 -0300

    arquivos para completar a parte de seleção da etapa6

---
+++ model/selecaogeral.py
@@ -0,0 +1,24 @@
+from model.figuras import Figuras
+
+class Selecaogeral(Figuras):
+    def __init__(self, ini_x, ini_y, posx, posy):
+        super().__init__(ini_x, ini_y, posx, posy)
+
+    def pegar_dados(self):
+        return (self.ini_x, self.ini_y, self.posx, self.posy)
+    
+    def validar(self):
+        return abs(self.posx - self.ini_x) >= 2 and abs(self.posy - self.ini_y) >= 2
+    
+    # verificar se o clique ocorreu dentro do retangulo, encontrando a largura e altura, ou seja, retornará True se tiver dentro dos limites do retângulo
+    def contem(self, x, y):
+        x_min, x_max = min(self.ini_x, self.posx), max(self.ini_x, self.posx)
+        y_min, y_max = min(self.ini_y, self.posy), max(self.ini_y, self.posy)
+        return x_min <= x <= x_max and y_min <= y <= y_max
+    
+    # deslocamento (somando a variação)
+    def mover(self, dx, dy):
+        self.ini_x += dx
+        self.ini_y += dy
+        self.posx += dx
+        self.posy += dy
